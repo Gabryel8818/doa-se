ad035fe998ec00f5b5a11518dae44419ca43927f
Gabriel Melo <gabriel_melo88@hotmail.com>
2021-09-20 21:56:08 -0300

    first commit

---
+++ frontend/js/doador.js
@@ -0,0 +1,16 @@
+const url_to_fetch = "http://ec2-18-231-54-129.sa-east-1.compute.amazonaws.com:8086/doador"
+const email = document.querySelector('#signin-email').value
+const senha = document.querySelector('#signin-password').value
+
+
+
+fetch(url_to_fetch,options)
+    .then(function(response){
+        response.json().then(function(data) {
+            var arr = console.log(data)
+            var count = Object.keys(arr).length
+            console.log(count)
+          });
+        }).catch(function(err) {
+          console.error('Failed retrieving information', err);
+        });
