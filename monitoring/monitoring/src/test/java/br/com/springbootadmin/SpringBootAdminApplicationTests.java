22fd329cd26b86ed615b41a5f7d42744c3a48dd1
Gabriel <gabriel.mendonca14@outlook.com>
2021-10-04 22:58:26 -0300

    monitoração da aplicação

---
+++ monitoring/monitoring/src/test/java/br/com/springbootadmin/SpringBootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springbootadmin;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
