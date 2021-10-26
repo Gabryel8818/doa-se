aa3a2a096ca21541eef56edc15d636b01765e9b4
joao1713 <69879160+joao1713@users.noreply.github.com>
2021-10-26 17:16:28 -0300

    botao de doação e o formulario de doação

---
+++ frontend/js/registerdoc.js
@@ -0,0 +1,90 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAlGREJPTOTx3eIsalRh8xvngnPFF5c-Ss",
+    authDomain: "doase-4611e.firebaseapp.com",
+    projectId: "doase-4611e",
+    storageBucket: "doase-4611e.appspot.com",
+    messagingSenderId: "389479966724",
+    appId: "1:389479966724:web:60b9123f18dab6342fad51",
+    measurementId: "G-L4NYQFYF69"
+  };
+
+
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  const auth = firebase.auth()
+
+
+  document.getElementById("button-registrardoc").addEventListener("click",function(event){
+    event.preventDefault()
+  });
+
+
+const url_to_fetch = "http://ec2-18-231-54-129.sa-east-1.compute.amazonaws.com:8086/doador"
+
+
+  // SIGNUP
+  function registrardoc(){
+        const value_nomedoc = document.querySelector('#signup-nomedoc').value
+        const value_email = document.querySelector('#signup-email').value
+        const value_nome = document.querySelector('#signup-nome').value
+        const value_telefone = document.querySelector('#signup-telefone').value
+        const value_cep = document.querySelector('#signup-cep').value
+        const value_logradouro = document.querySelector('#signup-logradouro').value
+        const value_estado = document.querySelector('#signup-estado').value
+        const value_cpf = document.querySelector('#signup-cpf').value
+        const value_categoria = document.querySelector('#signup-categoria').value
+        const value_cidade = document.querySelector('#signup-cidade').value
+        let dataDoacao = {
+          value_nomedoc: value_nomedoc,
+          categoria: value_categoria,
+          doador: {
+              nome: value_nome,
+              telefone: value_telefone,
+              email: value_email,
+              cep: value_cep,
+              estado: value_estado,
+              cidade: value_cidade,
+              logradouro: value_logradouro,
+              cpf: value_cpf
+          },
+         ong: {
+             nome: null,
+             telefone: null,
+             email: null,
+             cep: null,
+             estado: null,
+             cidade: null,
+             logradouro: null,
+             cnp: null
+         },
+        receptor: {
+            nome: null,
+            telefone: null,
+            email: null,
+            cep: null,
+            estado: null,
+            cidade: null,
+            logradouro: null
+        },        
+  }
+ 
+ 
+ 
+ 
+                 // options para o fetch
+                           options = {
+                               method: 'POST',
+                               mode: 'cors',
+                               body: JSON.stringify(dataDoacao), // passando os dados da lista para o POST
+                               headers: {"Content-Type": "application/json"}
+                           }
+ 
+                           fetch("http://ec2-52-67-195-32.sa-east-1.compute.amazonaws.com:8086/doacao", options)
+                             .then( response => response.json())
+                             .then(console.log("Doacao:"))
+                             .then(json => console.log(json))
+                             .catch(err => console.log(err))
+
+  }
