ebac8fe02296d38e9b729c2c624a9297b0906147
Gabriel Melo <gabriel_melo88@hotmail.com>
2021-10-28 17:14:30 -0300

    add tables for list ongs

--- frontend/js/ongs.js
+++ frontend/js/ongs.js
@@ -1,14 +1,33 @@
-var url_to_ongs = "http://ec2-52-67-195-32.sa-east-1.compute.amazonaws.com:8086/ong"
-
-async function list_ongs() {
-    const options =  {
-        method: 'GET',
-        mode: 'cors'
-      }
+async function loadIntoTable(url, table) {
+  const options =  {
+      method: 'GET',
+      mode: 'cors'
+    }
+  
+  const response = await fetch(url,options);
+  const data = await response.json()
+  data.forEach(ong => {
+    document.querySelector("table").innerHTML += `
+      <tbody>
+          <tr id="${ong.id_ong}">
+              <td>${ong.id_ong}</td>  
+              <td >${ong.nome}</td>
+              <td>${ong.telefone}</td>
+              <td>${ong.cep}</td>
+              <td>${ong.logradouro}</td>
+              <td>${ong.cnpj}</td>
+          </tr>
+      </tbody>
+          
     
-    const response = await fetch(url_to_ongs,options);
-    const data = await response.json()
-    console.log(document.getElementById(ongs))
+    `
+
+  });
+
+
+  
 }
 
-list_ongs()
+
+
+loadIntoTable("https://ec2-52-67-195-32.sa-east-1.compute.amazonaws.com:8086/ong", document.querySelector("table"));
