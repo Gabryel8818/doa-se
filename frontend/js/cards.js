8748e834fba9feee425a6a20c45350f2da5f2915
Gabriel Melo <gabriel_melo88@hotmail.com>
2021-10-28 16:37:33 -0300

    add nome and ong in cards

--- frontend/js/cards.js
+++ frontend/js/cards.js
@@ -8,6 +8,7 @@ async function loadIntoTable(url, table) {
     const response = await fetch(url,options);
     const data = await response.json()
     data.forEach(doacao => {
+      console.log(status)
       console.log(doacao)
       document.querySelector("table").innerHTML += `
         <tbody>
@@ -15,7 +16,9 @@ async function loadIntoTable(url, table) {
                 <td>${doacao.id_doacao}</td>  
                 <td >${doacao.nome}</td>
                 <td>${doacao.categoria}</td>
-                <td>${doacao.ong}</td>
+                <td>${doacao.ong.nome}</td>
+                 <td>${doacao.doador.nome}</td>
+                 <td><span id="a" class="badge badge-success">Dsisponível</span></td>
                 <td> <button id="button-registrar" class="btn btn-lg btn-primary btn-login text-uppercase fw-bold mb-2" onclick="receber_doacao(${doacao.id_doacao})">Receber</button>
                 <button id="button-registrar" class="btn btn-lg btn-danger btn-login text-uppercase fw-bold mb-2" onclick="deletar_doacao(${doacao.id_doacao})">Deletar</button> </td>
             </tr>
@@ -23,7 +26,10 @@ async function loadIntoTable(url, table) {
             
       
       `
+ 
     });
+
+
     
 }
 
