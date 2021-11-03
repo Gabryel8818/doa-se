2122de69b5e6c8194dd75daa7f957bee93bf8550
joao1713 <69879160+joao1713@users.noreply.github.com>
2021-11-03 17:31:27 -0300

    NOVO required no js

--- frontend/js/registerdoc.js
+++ frontend/js/registerdoc.js
@@ -37,6 +37,79 @@ const firebaseConfig = {
         const value_categoria = document.getElementById('categoria').value
         const value_cidade = document.querySelector('#signup-cidade').value
 
+        if (value_nomedoc == "") {
+          document.getElementById('erro-nomedoc').innerHTML = "*informe o nome da doação!";
+          document.getElementById('signup-nomedoc').focus();
+          return false;
+        }else{
+          document.getElementById('erro-nomedoc').innerHTML = "";
+        }
+
+        if (value_nome == "") {
+          document.getElementById('erro-nome').innerHTML = "*informe o Nome!";
+          document.getElementById('signup-nome').focus();
+          return false;
+        }else{
+          document.getElementById('erro-nome').innerHTML = "";
+        }
+
+        if (value_telefone == "") {
+          document.getElementById('erro-telefone').innerHTML = "*informe o Telefone!";
+          document.getElementById('signup-telefone').focus();
+          return false;
+        }else{
+          document.getElementById('erro-telefone').innerHTML = "";
+        }
+
+        if (value_email == "") {
+          document.getElementById('erro-email').innerHTML = "*informe o Email!";
+          document.getElementById('signup-email').focus();
+          return false;
+        }else{
+          document.getElementById('erro-email').innerHTML = "";
+        }
+
+
+        if (value_cep == "") {
+          document.getElementById('erro-cep').innerHTML = "*informe o CEP!";
+          document.getElementById('signup-cep').focus();
+          return false;
+        }else{
+          document.getElementById('erro-cep').innerHTML = "";
+        }
+
+        if (value_estado == "") {
+          document.getElementById('erro-estado').innerHTML = "*informe o estado!";
+          document.getElementById('signup-estado').focus();
+          return false;
+        }else{
+          document.getElementById('erro-estado').innerHTML = "";
+        }
+
+        if (value_cidade == "") {
+          document.getElementById('erro-cidade').innerHTML = "*informe a cidade!";
+          document.getElementById('signup-cidade').focus();
+          return false;
+        }else{
+          document.getElementById('erro-cidade').innerHTML = "";
+        }
+
+        if (value_logradouro == "") {
+          document.getElementById('erro-logradouro').innerHTML = "*informe o logradouro!";
+          document.getElementById('signup-logradouro').focus();
+          return false;
+        }else{
+          document.getElementById('erro-logradouro').innerHTML = "";
+        }
+
+        if (value_cpf == "") {
+          document.getElementById('erro-cpf').innerHTML = "*informe o cpf!";
+          document.getElementById('signup-cpf').focus();
+          return false;
+        }else{
+          document.getElementById('erro-cpf').innerHTML = "";
+        }
+
         let dataDoacao = {
           nome: value_nomedoc,
           categoria: value_categoria,
